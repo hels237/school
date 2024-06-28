10edebd4a14a6dc9c22e78c3cb02906dbe2b7d72
hels <m7hels@gmail.com>
2024-06-28 11:21:51 +0100

    add CourseController , InstructorDetailsControllers

---
+++ src/main/java/com/luv2code/shool/controller/CourseController.java
@@ -0,0 +1,9 @@
+package com.luv2code.shool.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/school/course")
+public class CourseController {
+}
